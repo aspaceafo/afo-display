1e438cffe196ba03eeefe57843939f53844ee622
nathan <njhauenstein@gmail.com>
2013-03-14 02:58:13 -0400

    mostly working version -- display view options fix
    changed classes it selects on website

--- afo_display/galleriaload.js
+++ afo_display/galleriaload.js
@@ -20,14 +20,14 @@ $(document).ready(function() {
 	            var currImg = current.original;
 	            var imgclass = $(currImg).attr('class'); //imgclass retreives current image's orignal classes
 				imgclass += ' ui-draggable';
-	            console.log(imgclass);
+	            //console.log(imgclass);
 				$(e.imageTarget).addClass(imgclass) //adds current image's original classes to current image 
 			 
 			    afo_queue_initQueueBox();
 				afo_queue_initAddLinks();
 				afo_queue_initDraggable();
 				afo_queue_initDroppable();
-				console.log('afo_queue_init');
+				//console.log('afo_queue_init');
 			 
 	        });
 	    },		
